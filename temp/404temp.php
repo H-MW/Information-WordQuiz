07b8793fc34de4d8e29782ff1498b33bdd6c38b6
H-MW <142008733+H-MW@users.noreply.github.com>
2024-06-28 22:33:00 +0900

    Add files via upload

---
+++ temp/404temp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>クイズ <?php echo htmlspecialchars($id) ?></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div  id="main">
+        問題 <?php echo $id ?> は見つかりませんでした
+    </div>
+</body>
+</html>
